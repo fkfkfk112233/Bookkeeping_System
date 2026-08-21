5420b024dde89889dd19816aeeb388513c7ae4c8
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-21 14:31:08 +0800

    Update Categories.jsx

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -1,11 +1,9 @@
 function Categories() {
-
-    return (
-        <div className="container mt-4">
-            <h1>分類管理</h1>
-        </div>
-    )
-
+  return (
+    <div className="container mt-4">
+      <h1>分類管理</h1>
+    </div>
+  );
 }
 
-export default Categories
+export default Categories;
