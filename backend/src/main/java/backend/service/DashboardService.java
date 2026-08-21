eb7dbf81552db7ad195786dec94da5975e19553c
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-21 13:28:32 +0800

    Update DashboardService.java

--- backend/src/main/java/backend/service/DashboardService.java
+++ backend/src/main/java/backend/service/DashboardService.java
@@ -3,9 +3,11 @@ package backend.service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import backend.dto.dashboard.CategoryAmountResponse;
 import backend.dto.dashboard.DashboardSummaryResponse;
 import backend.enums.TransactionType;
 import backend.repository.TransactionRepository;
@@ -47,5 +49,49 @@ public class DashboardService {
                 expense,
                 balance
         );
+    }
+    
+    public List<CategoryAmountResponse> getExpense(
+            LocalDate startDate,
+            LocalDate endDate) {
+
+        List<CategoryAmountResponse> results =
+                transactionRepository.sumAmountGroupByCategory(
+                        TransactionType.EXPENSE,
+                        startDate,
+                        endDate
+                );
+
+        results.forEach(result -> {
+
+            if (result.getCategoryId() == null) {
+                result.setCategoryName("未分類");
+            }
+
+        });
+
+        return results;
+    }
+    
+    public List<CategoryAmountResponse> getIncome(
+            LocalDate startDate,
+            LocalDate endDate) {
+
+        List<CategoryAmountResponse> results =
+                transactionRepository.sumAmountGroupByCategory(
+                        TransactionType.INCOME,
+                        startDate,
+                        endDate
+                );
+
+        results.forEach(result -> {
+
+            if (result.getCategoryId() == null) {
+                result.setCategoryName("未分類");
+            }
+
+        });
+
+        return results;
     }
 }
