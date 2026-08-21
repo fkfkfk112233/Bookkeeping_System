6795da5120a50c7156408ab69dfc65f84c5a8296
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-21 12:57:57 +0800

    Update Category.java

--- backend/src/main/java/backend/entity/Category.java
+++ backend/src/main/java/backend/entity/Category.java
@@ -2,8 +2,13 @@ package backend.entity;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import backend.enums.TransactionType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +18,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "categories")
 public class Category {
@@ -30,8 +36,10 @@ public class Category {
     @Enumerated(EnumType.STRING)
     private TransactionType type;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     public Category() {
