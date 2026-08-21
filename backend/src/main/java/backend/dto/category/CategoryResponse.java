c5226ded8cf299aa9b4470d5e53d1153685e9083
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-21 12:32:55 +0800

    Update CategoryResponse.java

--- backend/src/main/java/backend/dto/category/CategoryResponse.java
+++ backend/src/main/java/backend/dto/category/CategoryResponse.java
@@ -1,12 +1,14 @@
 package backend.dto.category;
 
+import backend.enums.TransactionType;
+
 public class CategoryResponse {
 
     private Long id;
 
     private String name;
 
-    private String type;
+    private TransactionType type;
 
     public CategoryResponse() {
     }
@@ -27,11 +29,11 @@ public class CategoryResponse {
         this.name = name;
     }
 
-    public String getType() {
+    public TransactionType getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setType(TransactionType transactionType) {
+        this.type = transactionType;
     }
 }
