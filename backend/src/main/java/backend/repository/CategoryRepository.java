d25030c35ac4a101dcd1ed34763c74826888e451
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-21 11:13:36 +0800

    Create CategoryRepository.java

---
+++ backend/src/main/java/backend/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import backend.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
