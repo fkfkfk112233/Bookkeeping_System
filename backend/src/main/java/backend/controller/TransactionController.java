c1efbfb43726584706827a1e5a5f1d4bd8b95cd3
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-21 11:14:09 +0800

    Create TransactionController.java

---
+++ backend/src/main/java/backend/controller/TransactionController.java
@@ -0,0 +1,35 @@
+package backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import backend.dto.transaction.TransactionRequest;
+import backend.dto.transaction.TransactionResponse;
+import backend.service.TransactionService;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionResponse> createTransaction(
+            @RequestBody TransactionRequest request) {
+
+        TransactionResponse response =
+                transactionService.createTransaction(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
