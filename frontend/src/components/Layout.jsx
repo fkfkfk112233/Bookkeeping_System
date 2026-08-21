11ed21b840a56c2cc86995ebb3cf98901d4fb351
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-21 14:26:26 +0800

    Create Layout.jsx

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
+
+function Layout() {
+
+    return (
+        <>
+            <Navbar />
+
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
+
+}
+
+export default Layout
