a72fc91cde50ddea248aaa36a25a8f287c47d9fb
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-21 11:13:52 +0800

    Create TransactionRequest.java

---
+++ backend/src/main/java/backend/dto/transaction/TransactionRequest.java
@@ -0,0 +1,70 @@
+package backend.dto.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class TransactionRequest {
+
+    private Long categoryId;
+
+    private String type;
+
+    private BigDecimal amount;
+
+    private String paymentMethod;
+
+    private String description;
+
+    private LocalDate transactionDate;
+
+    public TransactionRequest() {
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+}
