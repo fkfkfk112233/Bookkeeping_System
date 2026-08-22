bef8fc48ec6048bc2e8f6294db02c704190dff4a
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-23 03:30:47 +0800

    Update Category.java

--- backend/src/main/java/backend/entity/Category.java
+++ backend/src/main/java/backend/entity/Category.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import backend.enums.TransactionType;
