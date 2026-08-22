04316f7fa61beed3d7e3347e5859cb14b9bc6c8c
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-23 05:58:42 +0800

    Update categoryApi.js

--- frontend/src/services/categoryApi.js
+++ frontend/src/services/categoryApi.js
@@ -7,3 +7,11 @@ export function getCategories() {
 export function createCategory(category) {
   return api.post("/categories", category);
 }
+
+export function updateCategory(id, data) {
+  return api.put(`/categories/${id}`, data);
+}
+
+export function deleteCategory(id) {
+  return api.delete(`/categories/${id}`);
+}
