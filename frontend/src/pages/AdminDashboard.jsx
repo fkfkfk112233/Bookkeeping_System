fbd02549901a443e5084ac1ecf8c81a55bab4e72
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 13:58:09 +0800

    Create AdminDashboard.jsx

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+function AdminDashboard() {
+  return (
+    <div className="container py-5">
+      <h1>Admin Dashboard</h1>
+
+      <p className="text-body-secondary">歡迎進入後台管理系統</p>
+    </div>
+  );
+}
+
+export default AdminDashboard;
