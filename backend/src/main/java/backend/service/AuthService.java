5d09075367760d1a0cd54cf105a44eaa4f18a6f9
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 17:15:10 +0800

    Create AuthService.java

---
+++ backend/src/main/java/backend/service/AuthService.java
@@ -0,0 +1,22 @@
+package backend.service;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+	private final AuthenticationManager authenticationManager;
+
+	public AuthService(AuthenticationManager authenticationManager) {
+
+		this.authenticationManager = authenticationManager;
+	}
+
+	public Authentication authenticate(String username, String password) {
+
+		return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+	}
+}
