02666a3904a6935080df0bfaccb13459135f1aeb
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 17:15:22 +0800

    Update SecurityConfig.java

--- backend/src/main/java/backend/config/SecurityConfig.java
+++ backend/src/main/java/backend/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +18,12 @@ public class SecurityConfig {
 	}
 
 	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http.csrf(csrf -> csrf.disable())
@@ -25,7 +33,7 @@ public class SecurityConfig {
 						// Login
 						.requestMatchers("/api/auth/**").permitAll()
 
-						// 暫時全部 API 開放
+						// 暫時保留 API 開放
 						.requestMatchers("/api/**").permitAll()
 
 						.anyRequest().permitAll());
