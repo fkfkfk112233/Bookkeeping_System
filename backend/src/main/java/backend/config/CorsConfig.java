f0d3bb8ff67d3e617055210f58e5e59cbff004f4
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 15:57:03 +0800

    Update CorsConfig.java

--- backend/src/main/java/backend/config/CorsConfig.java
+++ backend/src/main/java/backend/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
                         "GET",
                         "POST",
                         "PUT",
+                        "PATCH",
                         "DELETE",
                         "OPTIONS")
                 .allowedHeaders("*");
