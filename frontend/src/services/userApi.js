097594fd85587c49442d9e9aefb5dc75ecea35c4
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 14:14:18 +0800

    Create userApi.js

---
+++ frontend/src/services/userApi.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/admin/users";
+
+export const getUsers = async () => {
+  const response = await axios.get(API_URL);
+
+  return response.data;
+};
