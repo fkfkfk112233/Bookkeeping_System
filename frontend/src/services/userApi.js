425b093fa5f45de72a246dffd3056b08d9370b87
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 15:56:45 +0800

    Update userApi.js

--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -1,8 +1,8 @@
 import api from "./api";
 
+// Admin
 export const getUsers = async () => {
   const response = await api.get("/admin/users");
-
   return response.data;
 };
 
@@ -14,6 +14,7 @@ export const createUser = async (userData) => {
 
 export const updateUser = async (id, userData) => {
   const response = await api.put(`/admin/users/${id}`, userData);
+
   return response.data;
 };
 
@@ -21,10 +22,26 @@ export const deleteUser = async (id) => {
   await api.delete(`/admin/users/${id}`);
 };
 
+// User Login
 export const getUserByUsername = async (username) => {
-  const response = await api.get(
-    `/users/username/${username}`
-  );
+  const response = await api.get(`/users/username/${username}`);
 
   return response.data;
+};
+
+// User Profile
+export const getUserProfile = async (id) => {
+  const response = await api.get(`/users/${id}`);
+
+  return response.data;
+};
+
+export const updateUserProfile = async (id, userData) => {
+  const response = await api.put(`/users/${id}`, userData);
+
+  return response.data;
+};
+
+export const disableUser = async (id) => {
+  await api.patch(`/users/${id}/disable`);
 };
