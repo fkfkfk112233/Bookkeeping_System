4ab723567c658ed4c29f78db27021b7f41739a42
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 14:10:08 +0800

    Update CategoryRepository.java

--- backend/src/main/java/backend/repository/CategoryRepository.java
+++ backend/src/main/java/backend/repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import backend.entity.Category;
+import backend.entity.User;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+	boolean existsByUser(User user);
 }
