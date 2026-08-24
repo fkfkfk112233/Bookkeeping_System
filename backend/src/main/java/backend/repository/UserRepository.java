88d3a6754345d7b39112a69e8aefc1f20135cc06
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 15:56:59 +0800

    Update UserRepository.java

--- backend/src/main/java/backend/repository/UserRepository.java
+++ backend/src/main/java/backend/repository/UserRepository.java
@@ -2,8 +2,11 @@ package backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 import backend.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUsername(String username);
 }
