3393b6c5e614178408750ea2355bcc585cedc612
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 15:36:14 +0800

    Create UserProfileRequest.java

---
+++ backend/src/main/java/backend/dto/user/UserProfileRequest.java
@@ -0,0 +1,37 @@
+package backend.dto.user;
+
+public class UserProfileRequest {
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    public UserProfileRequest() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
