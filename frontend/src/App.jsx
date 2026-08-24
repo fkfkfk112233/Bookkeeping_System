14d35c8f0fd45fed46c111efdc236c3ee90d71be
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-24 13:58:22 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout";
+import AdminLayout from "./components/AdminLayout";
 
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
 import Categories from "./pages/Categories";
 
+import Login from "./pages/Login";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserManagement from "./pages/admin/UserManagement";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Login */}
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/login/admin" element={<AdminLogin />} />
+
+        {/* User System */}
         <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} />
 
           <Route path="/transactions" element={<Transactions />} />
 
           <Route path="/categories" element={<Categories />} />
+        </Route>
+
+        {/* Admin System */}
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminDashboard />} />
+
+          <Route path="/admin/users" element={<UserManagement />} />
         </Route>
       </Routes>
     </BrowserRouter>
