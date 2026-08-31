bf36ddbbecc2767a2a9d0865188c95a2ee34dcb5
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-31 17:10:49 +0800

    Update CategoryRepository.java

--- backend/src/main/java/backend/repository/CategoryRepository.java
+++ backend/src/main/java/backend/repository/CategoryRepository.java
@@ -1,5 +1,8 @@
 package backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import backend.entity.Category;
@@ -7,5 +10,13 @@ import backend.entity.User;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-	boolean existsByUser(User user);
+    boolean existsByUser(User user);
+
+    List<Category> findByUserIsNullOrUserOrderByTypeAscNameAsc(User user);
+
+    Optional<Category> findByIdAndUser(Long id, User user);
+
+    Optional<Category> findByIdAndUserIsNull(Long id);
+
+    boolean existsByUserIsNull();
 }
