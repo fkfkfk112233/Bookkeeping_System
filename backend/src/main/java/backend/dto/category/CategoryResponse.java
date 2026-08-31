0cfd562080ca65c91db41f8dbb815d6d211a5482
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-31 17:10:55 +0800

    Update CategoryResponse.java

--- backend/src/main/java/backend/dto/category/CategoryResponse.java
+++ backend/src/main/java/backend/dto/category/CategoryResponse.java
@@ -5,35 +5,22 @@ import backend.enums.TransactionType;
 public class CategoryResponse {
 
     private Long id;
-
     private String name;
-
     private TransactionType type;
+    private boolean defaultCategory;
 
     public CategoryResponse() {
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
+    public TransactionType getType() { return type; }
+    public void setType(TransactionType transactionType) { this.type = transactionType; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public TransactionType getType() {
-        return type;
-    }
-
-    public void setType(TransactionType transactionType) {
-        this.type = transactionType;
-    }
+    public boolean isDefaultCategory() { return defaultCategory; }
+    public void setDefaultCategory(boolean defaultCategory) { this.defaultCategory = defaultCategory; }
 }
